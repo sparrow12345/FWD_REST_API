6117096f94d338aa86a4dc4ee0d89ff75ef5403d
sparrow12345 <m.naser@innopolis.university>
2023-11-11 00:29:42 +0300

    fifth

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "json-server";
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
