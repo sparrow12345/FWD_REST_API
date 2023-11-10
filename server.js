00901d2326beea3f1061a8aa2efb3619230b9427
sparrow12345 <m.naser@innopolis.university>
2023-11-11 00:27:56 +0300

    fourth

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-// server.use(
-// 	// Add custom route here if needed
-// 	jsonServer.rewriter({
-// 		"/api/*": "/$1",
-// 	})
-// );
+server.use(
+	// Add custom route here if needed
+	jsonServer.rewriter({
+		"/api/*": "/$1",
+	})
+);
 server.use(router);
 server.listen(3000, () => {
 	console.log("JSON Server is running");
